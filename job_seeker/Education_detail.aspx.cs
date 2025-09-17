2891c2a7cc36e6ea01a46e6956339365e066e5eb
Admin <Admin@DESKTOP-OV6MRB9>
2025-09-17 11:44:39 +0530

    Revert "HII"
    This reverts commit 004e6c47a1bfb394c104542b4d813154190d2d8f.

--- job_seeker/Education_detail.aspx.cs
+++ job_seeker/Education_detail.aspx.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,120 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace job_portal.job_seeker
 {
-   
     public partial class Education_detail : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\job_portel\\App_Data\\jobportal.mdf;Integrated Security=True";
 
-            if (Session["Username"] != null && Session["UserRole"].ToString() == "Jobseeker")
-            {
-                string username = Session["Username"].ToString();
-
-            }
-            else
-            {
-                Response.Redirect("~/login_page.aspx");
-            }
-        }
-
-        protected void btnSubmittt_Click(object sender, EventArgs e)
-        {
-            //if (Session["Username"] == null)
-            //{
-            //    Response.Redirect("~/login_page.aspx"); // Redirect if session data is missing
-            //    return;
-            //}
-
-            // Retrieve the username from session
-            string username = Session["Username"].ToString();
-            string seekerId = GetSeekerId(username); // Get seekerid from database
-
-            if (string.IsNullOrEmpty(seekerId))
-            {
-                Response.Write("Error fetching user details. Please log in again.");
-                // lblMessage.ForeColor = System.Drawing.Color.Red;
-                return;
-            }
-
-            // Retrieve user input
-            string university = ddlUniversity.SelectedValue;
-            string field = ddlField.SelectedValue;
-            string degree = ddlDegree.SelectedValue;
-            string graduationYear = ddlYear.SelectedValue;
-            string ugpa = txtUgpa.Text;
-
-            // Validate inputs
-            if (university == "---Select University" || field == "---Select Your Course" || degree == "---Select Degree")
-            {
-                Response.Write("Please select all required fields.");
-                // lblMessage.ForeColor = System.Drawing.Color.Red;
-                return;
-            }
-
-            // Database connection
-            string connString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
-
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connString))
-                {
-                    string query = "INSERT INTO tbl_educationdetails (seekerid, university, degree, major, graduationyear, gpa) " +
-                                   "VALUES (@seekerid, @university, @degree, @major, @graduationyear, @gpa)";
-
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@seekerid", seekerId);
-                        cmd.Parameters.AddWithValue("@university", university);
-                        cmd.Parameters.AddWithValue("@degree", degree);
-                        cmd.Parameters.AddWithValue("@major", field);
-                        cmd.Parameters.AddWithValue("@graduationyear", graduationYear);
-                        cmd.Parameters.AddWithValue("@gpa", ugpa);
-
-                        conn.Open();
-                        int rowsAffected = cmd.ExecuteNonQuery();
-
-                        if (rowsAffected > 0)
-                        {
-                            // Redirect to profile page after successful insertion
-                            Response.Redirect("~/Employee/Employee_profile.aspx");
-                        }
-                        else
-                        {
-                            Response.Write("Error inserting data. Please try again.");
-                            //lblMessage.ForeColor = System.Drawing.Color.Red;
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                //  lblMessage.Text = "Database Error: " + ex.Message;
-                //lblMessage.ForeColor = System.Drawing.Color.Red;
-            }
-        }
-
-        private string GetSeekerId(string username)
-        {
-            string seekerId = "";
-            string connString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connString))
-            {
-                string query = "SELECT seekerid FROM tbl_jobseeker WHERE username = @username";
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@username", username);
-                    conn.Open();
-                    object result = cmd.ExecuteScalar();
-                    if (result != null)
-                    {
-                        seekerId = result.ToString();
-                    }
-                }
-            }
-            return seekerId;
         }
     }
 }
--- job_seeker/applied_job.aspx.cs
+++ job_seeker/applied_job.aspx.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,84 +12,6 @@ namespace job_portal.job_seeker
         protected void Page_Load(object sender, EventArgs e)
         {
 
-            if (Session["Username"] != null && Session["UserRole"].ToString() == "Jobseeker")
-            {
-                string username = Session["Username"].ToString();
-
-            }
-            else
-            {
-                Response.Redirect("~/login_page.aspx");
-            }
-            if (!IsPostBack)
-            {
-                LoadAppliedJobs();
-            }
-        }
-
-        private void LoadAppliedJobs()
-        {
-            if (Session["username"] == null)
-            {
-                Response.Write("You need to log in as a job seeker to view applied jobs.");
-                // lblMessage.ForeColor = System.Drawing.Color.Red;
-                return;
-            }
-
-            string username = Session["username"].ToString();
-            string connString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
-            int seekerId = 0;
-
-            using (SqlConnection con = new SqlConnection(connString))
-            {
-                con.Open();
-
-                // ✅ Retrieve seekerid from username
-                string getSeekerIdQuery = "SELECT seekerid FROM tbl_jobseeker WHERE username = @username";
-                using (SqlCommand cmd = new SqlCommand(getSeekerIdQuery, con))
-                {
-                    cmd.Parameters.AddWithValue("@username", username);
-                    object result = cmd.ExecuteScalar();
-
-                    if (result == null)
-                    {
-                        Response.Write("Invalid job seeker account.");
-                        // lblMessage.ForeColor = System.Drawing.Color.Red;
-                        return;
-                    }
-                    seekerId = Convert.ToInt32(result);
-                }
-
-                // ✅ Now fetch applied jobs using seekerid
-                string query = @"SELECT ja.jobpostid, jp.jobtitle, ja.applicationdate, ja.status 
-                             FROM tbl_jobapplication ja
-                             INNER JOIN tbl_jobpost jp ON ja.jobpostid = jp.jobpostid
-                             WHERE ja.seekerid = @seekerid";
-
-                using (SqlCommand cmd = new SqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("@seekerid", seekerId);
-                    cmd.Parameters.AddWithValue("@status", "pending");
-                    SqlDataAdapter da = new SqlDataAdapter(cmd);
-                    DataTable dt = new DataTable();
-                    da.Fill(dt);
-
-                    if (dt.Rows.Count > 0)
-                    {
-                        rptAppliedJobs.DataSource = dt;
-                        rptAppliedJobs.DataBind();
-                    }
-                    else
-                    {
-                        Response.Write("No jobs applied yet.");
-                        //lblMessage.ForeColor = System.Drawing.Color.Gray;
-                    }
-                }
-            }
-        }
-        protected void btnApplyNew_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("Jobs.aspx");
         }
     }
 }
