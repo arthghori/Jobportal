cbb797df0cf0d71f707d1598228577d03e26d242
admin <admin@ARTHGHORI>
2025-10-31 00:56:54 +0530

    skill

--- job_seeker/Employee_profile.aspx.designer.cs
+++ job_seeker/Employee_profile.aspx.designer.cs
@@ -330,21 +330,12 @@ namespace job_portal.job_seeker
         protected global::System.Web.UI.WebControls.Repeater rptSkills;
 
         /// <summary>
-        /// gvEducation control.
+        /// lblNoSkills control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvEducation;
-
-        /// <summary>
-        /// btnManageEducation control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnManageEducation;
+        protected global::System.Web.UI.WebControls.Label lblNoSkills;
     }
 }
